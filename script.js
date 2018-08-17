684c78b1daa7b109deaad883b117b2edc201b520
bgongre <27456325+bgongre@users.noreply.github.com>
2018-08-17 13:45:23 -0400

    added mailchimp form

---
+++ script.js
@@ -0,0 +1,8 @@
+let navbar = document.getElementById("navbar");
+
+
+const addScrollingClass = () => {
+  navbar.classList.add("navbar-color-on-scroll");
+}
+
+window.addEventListener("scroll", addScrollingClass);
